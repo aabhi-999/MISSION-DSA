cb10542427933abed84a762af1f2f632931ad483
Abhinav kumar mishra <2k23.cscys2310845@gmail.com>
2025-05-12 16:18:11 +0530

    Create waiter.py

---
+++ hackerrank/waiter.py
@@ -0,0 +1,69 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'waiter' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY number
+#  2. INTEGER q
+#
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def first_n_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+
+def waiter(number, q):
+    primes = first_n_primes(q)
+    answers = []
+    arr = number
+    for p in primes:
+        B=[]
+        A =[]
+        for e in arr:
+            if e % p == 0:
+                B.append(e)
+            else:
+                A.append(e)
+        answers+=B
+        arr = A[::-1]
+    answers +=A
+    return answers
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    number = list(map(int, input().rstrip().split()))
+
+    result = waiter(number, q)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
