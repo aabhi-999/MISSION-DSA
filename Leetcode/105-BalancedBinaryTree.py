7081eba9a3f29131c84cc8e8da84f0db866ff66d
Abhinav kumar mishra <2k23.cscys2310845@gmail.com>
2025-11-07 09:18:33 +0530

    Update 105-BalancedBinaryTree.py

--- Leetcode/105-BalancedBinaryTree.py
+++ Leetcode/105-BalancedBinaryTree.py
@@ -8,7 +8,7 @@
 # class TreeNode: 
 #     def __init__(self, val=0, left=None, right=None): 
 #         self.val = val
-#         self.left = left  
+#         self.left = left   
 #         self.right = right     
 class Solution:     
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
