67165e601e8fc9e68091de4a5c36368192b1b413
Abhinav kumar mishra <2k23.cscys2310845@gmail.com>
2026-04-10 21:29:38 +0530

    Update 105-BalancedBinaryTree.py

--- Leetcode/105-BalancedBinaryTree.py
+++ Leetcode/105-BalancedBinaryTree.py
@@ -2,7 +2,7 @@
  class TreeNode:
      def __init__(self, val=0, left=None, right=None):  
          self.val = val
-         self.left = left
+         self.left = left 
          self.right = right  
  Definition for a binary tree node.       
  class TreeNode:   
