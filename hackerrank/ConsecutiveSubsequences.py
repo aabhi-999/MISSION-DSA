f0887a705354ef8f2a8cee4edc703f1ed7d1b818
Abhinav kumar mishra <2k23.cscys2310845@gmail.com>
2026-04-21 21:26:17 +0530

    Update ConsecutiveSubsequences.py

--- hackerrank/ConsecutiveSubsequences.py
+++ hackerrank/ConsecutiveSubsequences.py
@@ -3,7 +3,7 @@ for _ in range(int(input())):
     list1=list(map(int,input().split()))
     dict1={0:1}
     sum1,res=0,0 
-    for i in range(len(list1)):
+    for i in range(len(list1)): 
         sum1+=list1[i]  
         t=sum1%k  
         if t in dict1: 
